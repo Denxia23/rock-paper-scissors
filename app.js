b9388bae0b27c205e6585a72fda9afe4ec523a2f
Denxia23 <dsoka223@gmail.com>
2022-10-11 12:19:38 -0300

    edit index.html, styles.css and app.js

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ function playRound(playerChoice, computerChoice) {
   return roundWinner;
 };
 
+function updateInfo() {
+  roundN.innerText = `Round ${roundNum}`;
+  pScore.innerText = playerScore;
+  cScore.innerText = computerScore;
+};
+
 function game(playerChoice) {
   let computerChoice = getComputerSelection();
   cSelectionImg.src = `images/${computerChoice}.png`;
@@ -36,13 +42,11 @@ function game(playerChoice) {
       console.log("You Win! " + playerChoice + " Beats " + computerChoice);
       playerScore++;
       cSelectionDiv.classList.add("lose");
-      pScore.innerText = playerScore;
       break;
     case "computer":
       console.log("You Lose! " + computerChoice + " Beats " + playerChoice);
       computerScore++;
       pSelectionDiv.classList.add("lose");
-      cScore.innerText = computerScore;
       break;
     case "none":
       console.log("It's a Tie!");
@@ -51,26 +55,11 @@ function game(playerChoice) {
       break;
   };
   roundNum++;
-  round.innerText = `Round ${roundNum}`;
+  updateInfo();
   roundSect.addEventListener("transitionend", removeTransitionClass);
-  
   if (playerScore === 5 || computerScore === 5) {
     gameEnd();
   };
-};
-
-
-function gameEnd() {
-  let messages = ["You Won, congratulations!", "You lost, better luck next time!"]
-  selectionSect.classList.add("fadeOut");
-  if (playerScore > computerScore) {
-    endMsg.innerText = messages[0];
-  } else {
-    endMsg.innerText = messages[1];
-  };
-  gameEndSect.classList.add("transition");
-
-
 };
 
 function removeTransitionClass(e) {
@@ -81,32 +70,48 @@ function removeTransitionClass(e) {
   cSelectionDiv.classList.remove("lose");
 };
 
+function gameEnd() {
+  let messages = ["You Won, congratulations!", "You lost, better luck next time!"]
+  selectionSect.classList.add("fadeOut");
+  if (playerScore > computerScore) {
+    endMsg.innerText = messages[0];
+  } else {
+    endMsg.innerText = messages[1];
+  };
+  gameEndSect.classList.add("transition");
+};
+
 
 let playerScore = 0;
 let computerScore = 0;
 let roundNum = 1;
+//intro
+const textDiv = document.querySelector("#game-intro .text");
+const circles = document.querySelector("#game-intro .circles");
+const wave = document.querySelector("#game-intro .wave");
+//game info
+const pScore = document.querySelector("#game-info #p-points");
+const cScore = document.querySelector("#game-info #c-points");
+const playButton = document.querySelector("#game-intro .play.btn");
+const roundN = document.querySelector("#game-info .round");
+//selection
 const rockDiv = document.querySelector("#selection #rock");
 const paperDiv = document.querySelector("#selection #paper");
 const scissorsDiv = document.querySelector("#selection #scissors");
-const pScore = document.querySelector("#game-info #p-points");
-const cScore = document.querySelector("#game-info #c-points");
-const playButton = document.querySelector("#game-intro .play-btn");
-const textDiv = document.querySelector("#game-intro .text");
 const gameSect = document.querySelector("#game");
-const round = document.querySelector("#game-info .round");
-const circles = document.querySelector("#game-intro .circles");
 const selectionSect = document.querySelector("#game #selection");
+//round
 const roundSect = document.querySelector("#game #round");
 const pSelectionImg = document.createElement("img");
 const cSelectionImg = document.createElement("img");
 const pSelectionDiv = document.querySelector("#game #round .player-selection");
 const cSelectionDiv = document.querySelector("#game #round .cpu-selection");
-const wave = document.querySelector("#game-intro .wave");
+//game end
 const endMsg = document.querySelector("#game #game-end .end-msg");
 const gameEndSect = document.querySelector("#game #game-end");
+const replayButton = document.querySelector("#game #game-end .replay.btn");
 
 function main() {
-  console.log(playerScore);
   playButton.addEventListener("click", () => {
     textDiv.classList.add("transition");
     gameSect.classList.add("transition");
@@ -114,6 +119,15 @@ function main() {
     wave.classList.add("transition");
   });
 
+  replayButton.addEventListener("click", () => {
+    gameEndSect.classList.remove("transition");
+    selectionSect.classList.remove("fadeOut");
+    playerScore = 0;
+    computerScore = 0;
+    roundNum = 1;
+    updateInfo();
+  })
+  
   circles.addEventListener("transitionend", () => {
     circles.replaceChildren();
   });
